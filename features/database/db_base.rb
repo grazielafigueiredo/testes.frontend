5fd53f22f452c1cffd9aade0a944dfdd748eb70a
Graziela <graziela.afigueiredo@gmail.com>
2020-10-14 17:15:53 -0300

    install banco de dados

---
+++ features/database/db_base.rb
@@ -0,0 +1,18 @@
+require 'tiny_tds'
+require 'dotenv'
+
+class DbBase
+  def initialize
+    azure_bool = ENV['DATABASE_AZURE'].to_s.downcase == 'true'
+    conn = {
+      username: ENV['DATABASE_USERNAME'],
+      password: ENV['DATABASE_PASSWORD'],
+      host: ENV['DATABASE_HOST'],
+      port: ENV['DATABASE_PORT'],
+      database: ENV['DATABASE_DATABASE'],
+      azure: azure_bool,
+      timeout: ENV['DATABASE_TIMEOUT']
+    }
+    @connection = TinyTds::Client.new(conn)
+  end
+end
