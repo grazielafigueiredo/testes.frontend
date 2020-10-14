15e8130b152731ecc0477b97278a6c05becdc02b
Graziela <graziela.afigueiredo@gmail.com>
2020-10-14 17:16:53 -0300

    add headless ao teste

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,42 +12,34 @@ require 'version'
 require 'tiny_tds'
 
 require_relative 'env'
+require_relative 'databases/transfer_DB'
 
 
 World(Capybara::DSL)
 World(Capybara::RSpecMatchers)
 
-# Selenium::WebDriver::Chrome.driver_path = 'C:\Users\Graziela\Downloads\primeiro_projeto\chromedriver_win32\chromedriver'
-
-# Capybara.register_driver :selenium do |app|
-#     profile = Selenium::WebDriver::Firefox::Profile.new
-#     profile.native_events = true
-#     Capybara::Selenium::Driver.new(
-#         app,
-#         :browser => :firefox, profile: profile
-#         )
-#     Capybara::Selenium::Driver.new(
-#         app,
-#         browser: :chrome,
-#         desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-#             'chromeOptions' => {'args' => [
-#                 '--disable-infobars','window-size=1600,1024'
-#                 ]}
-#         )
-#     )
-# end
 Capybara.register_driver :site_prism do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+Capybara.register_driver :selenium_chrome_headless_docker_friendly do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  # browser_options.args << '--headless'
+  # browser_options.args << '--disable-gpu'
+  # browser_options.args << '--start-maximized'
+  browser_options.args << '--enable-features=NetworkService,NetworkServiceInProcess'
+  # Sandbox cannot be used inside unprivileged Docker container
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--disable-dev-shm-usage'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
 Capybara.configure do |config|
   config.run_server = false
-  config.default_driver = :site_prism
+  config.default_driver = :selenium_chrome_headless_docker_friendly 
+  config.javascript_driver = :selenium_chrome_headless_docker_friendly
   Capybara.page.driver.browser.manage.window.maximize
-  config.default_max_wait_time = 30
-  config.app_host = 'https://qa2.lottocap.com.br'
-
-  # config.before(:example) do 
-  #   page.current_windown.resize_to(1280, 800)
-  # end
+  config.default_max_wait_time = 10
+  config.app_host = 'https://dev1.lottocap.com.br'
 end
